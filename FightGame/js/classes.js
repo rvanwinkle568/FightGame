182d42f8a06a72811d98c1edd29c66725b830b21
rvanwinkle568 <rayhan731@outlook.com>
2024-06-26 13:38:22 -0700

    Update classes.js
    Added attack animation switch case, fixed falling/idle animation bug and got rid of attack time out

--- FightGame/js/classes.js
+++ FightGame/js/classes.js
@@ -70,7 +70,7 @@
 
 class Fighter extends Sprite
 {
-  constructor({position, velocity, color = "red", imageSrc, scale = 1,           framesMax = 1, offset = {x: 0, y: 0}, sprites})
+  constructor({position, velocity, color = "red", imageSrc, scale = 1,           framesMax = 1, offset = {x: 0, y: 0}, sprites, attackBox = {offset: {x: 0, y: 0}, width: undefined, height: undefined}})
   {
     super({
       position,
@@ -90,9 +90,9 @@ class Fighter extends Sprite
         x: this.position.x,
         y: this.position.y
       },
-      offset,
-      height: 50,
-      width: 100
+      offset: attackBox.offset,
+      height: attackBox.height,
+      width: attackBox.width
     }
     this.color = color
     this.isAttacking
@@ -119,14 +119,18 @@ class Fighter extends Sprite
     this.animateFrames()
 
     this.attackBox.position.x = this.position.x + this.attackBox.offset.x
-    this.attackBox.position.y = this.position.y
+    this.attackBox.position.y = this.position.y + this.attackBox.offset.y
 
+    //For drawing attack box
+    // c.fillRect(this.attackBox.position.x, this.attackBox.position.y,                  this.attackBox.width, this.attackBox.height)
+    
     this.position.x += this.velocity.x
     this.position.y += this.velocity.y
 
     if (this.position.y + this.height + this.velocity.y >= canvas.height - 96)
     {
       this.velocity.y = 0
+      this.position.y = 330
       this.isGrounded = true
     } 
     else
@@ -138,21 +142,20 @@ class Fighter extends Sprite
 
   attack()
   {
+    this.switchSprite("attack1")
     this.isAttacking = true
-    setTimeout(() => 
-      {
-      this.isAttacking = false
-      }, 100)
   }
 
   switchSprite(sprite)
   {
+    if (this.image === this.sprites.attack1.image && 
+        this.currFrame < this.sprites.attack1.framesMax - 1) return
     switch(sprite)
     {
       case "idle":
         if (this.image !== this.sprites.idle.image)
         {
-          this.image = player.sprites.idle.image
+          this.image = this.sprites.idle.image
           this.framesMax = this.sprites.idle.framesMax
           this.currFrame = 0
         }
@@ -160,7 +163,7 @@ class Fighter extends Sprite
       case "run":
         if (this.image !== this.sprites.run.image)
         {
-          this.image = player.sprites.run.image
+          this.image = this.sprites.run.image
           this.framesMax = this.sprites.run.framesMax
           this.currFrame = 0
         }
@@ -178,6 +181,14 @@ class Fighter extends Sprite
         {
           this.image = this.sprites.fall.image
           this.framesMax = this.sprites.fall.framesMax
+          this.currFrame = 0
+        }
+      break
+      case "attack1":
+        if (this.image !== this.sprites.attack1.image)
+        {
+          this.image = this.sprites.attack1.image
+          this.framesMax = this.sprites.attack1.framesMax
           this.currFrame = 0
         }
       break
