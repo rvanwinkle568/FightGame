46c943279a3c1599e03aeef06f49931bc51f228f
rvanwinkle568 <rayhan731@outlook.com>
2024-06-25 18:37:34 -0700

    Update classes.js
    Fixed jump animation bug. Added fall animation

--- FightGame/js/classes.js
+++ FightGame/js/classes.js
@@ -1,61 +1,72 @@
-class Sprite
-{
-  constructor({position, imageSrc, scale = 1, framesMax = 1, 
-  offset = {x: 0, y: 0}})
+  class Sprite 
   {
-    this.position = position
-    this.width = 50
-    this.height = 150
-    this.image = new Image()
-    this.image.src = imageSrc
-    this.scale = scale
-    this.frameMax = framesMax
-    this.currFrame = 0        //current frame
-    this.elapsedFrame = 0     //amount of frames elapsed
-    this.holdFrame = 5       //holds the frame for a certain amount of time
-    this.offset = offset
-  }
-
-  draw()
-  {
-    c.drawImage(
-      //Cropping
-      this.image,
-      this.currFrame * (this.image.width / this.frameMax),
-      0,
-      this.image.width / this.frameMax,
-      this.image.height,
-      
-      this.position.x - this.offset.x, 
-      this.position.y - this.offset.y, 
-      (this.image.width / this.frameMax) * this.scale,
-      this.image.height * this.scale
-    )
-  }
-
-  animateFrames()
-  {
-    this.elapsedFrame++
-    if (this.elapsedFrame % this.holdFrame === 0) 
+    constructor({ position, imageSrc, scale = 1, framesMax = 1, 
+    offset = { x: 0, y: 0 } }) 
     {
-      if(this.currFrame < this.frameMax - 1)
+      this.position = position
+      this.image = new Image()
+      this.image.src = imageSrc
+      this.scale = scale
+      this.framesMax = framesMax
+      this.currFrame = 0
+      this.elapsedFrame = 0
+      this.holdFrame = 5
+      this.offset = offset
+    }
+
+    draw() 
+    {
+      if (this.framesMax > 1) 
       {
-        this.currFrame++
-      }
-      else
-      {
-        this.currFrame = 0
+        // For animated sprites
+        c.drawImage(
+          this.image,
+          this.currFrame * (this.image.width / this.framesMax),
+          0,
+          this.image.width / this.framesMax,
+          this.image.height,                 
+          this.position.x - this.offset.x,                  
+          this.position.y - this.offset.y,                  
+          (this.image.width / this.framesMax) * this.scale, 
+          this.image.height * this.scale 
+        )
+      } else {
+        // For static sprites
+        c.drawImage(
+          this.image,
+          this.position.x - this.offset.x, 
+          this.position.y - this.offset.y, 
+          this.image.width * this.scale, 
+          this.image.height * this.scale 
+        )
       }
     }
+
+    animateFrames() 
+    {
+      if (this.framesMax > 1) 
+      {
+        this.elapsedFrame++;
+        if (this.elapsedFrame % this.holdFrame === 0) 
+        {
+          if (this.currFrame < this.framesMax - 1) 
+          {
+            this.currFrame++;
+          } 
+          else 
+          {
+            this.currFrame = 0;
+          }
+        }
+      }
+    }
+
+    update() 
+    {
+      this.draw();
+      this.animateFrames();
+    }
   }
-  
-  update()
-  {
-    this.draw()
-    this.animateFrames()
-   
-  }
-}
 
 class Fighter extends Sprite
 {
@@ -99,14 +110,14 @@ class Fighter extends Sprite
     }
 
     console.log(this.sprites)
-      
+
   }
 
   update()
   {
     this.draw()
     this.animateFrames()
-    
+
     this.attackBox.position.x = this.position.x + this.attackBox.offset.x
     this.attackBox.position.y = this.position.y
 
@@ -159,6 +170,14 @@ class Fighter extends Sprite
         {
           this.image = this.sprites.jump.image
           this.framesMax = this.sprites.jump.framesMax
+          this.currFrame = 0
+        }
+      break
+      case "fall":
+        if (this.image !== this.sprites.fall.image)
+        {
+          this.image = this.sprites.fall.image
+          this.framesMax = this.sprites.fall.framesMax
           this.currFrame = 0
         }
       break
